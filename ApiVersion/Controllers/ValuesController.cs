9af4a1b15ab638ead8b4eb41e2a5fbec5fde8674
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-18 23:22:33 +0300

    api version example

---
+++ ApiVersion/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersion.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("merhaba api versionlama");
+        }
+    }
+}
