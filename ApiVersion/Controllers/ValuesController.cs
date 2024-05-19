b6ae41f8823c6ae726b5de7d59c11030090a3bf1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-19 13:29:21 +0300

    bitti.

--- ApiVersion/Controllers/ValuesController.cs
+++ ApiVersion/Controllers/ValuesController.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiVersion.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("GetAll")]
+        [ApiVersion("1", Deprecated = true)]
         public IActionResult GetAll()
         {
-            return Ok("merhaba api versionlama");
+            return Ok("V1 GetAll Result");
+        }
+
+        [HttpGet("GetAll")]
+        [ApiVersion("2")]
+        public IActionResult GetAllV2()
+        {
+            return Ok("V2 GetAll Result");
         }
     }
 }
--- ApiVersion/Program.cs
+++ ApiVersion/Program.cs
@@ -1,36 +1,42 @@
+using Asp.Versioning;
 
-namespace ApiVersion
+internal class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+
+        builder.Services.AddApiVersioning(action =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            action.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+            action.AssumeDefaultVersionWhenUnspecified = true;
+            action.ReportApiVersions = true;
+            action.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader("api-version"),
+                new HeaderApiVersionReader("X-Version"),
+                new MediaTypeApiVersionReader("ver")
+                );
+        });
 
-            // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
